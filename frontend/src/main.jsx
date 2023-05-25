6bf32439bdf84102a2dab6da6c718ce4cb5f0b4e
Himank Barve <hbarve1592@gmail.com>
2023-05-25 08:46:15 +0530

    First Commit

---
+++ frontend/src/main.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { configureChains, mainnet, WagmiConfig, createClient } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import { polygonMumbai } from "@wagmi/chains";
+
+const { provider, webSocketProvider } = configureChains(
+  [mainnet, polygonMumbai],
+  [publicProvider()],
+);
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <WagmiConfig client={client}>
+      <App />
+    </WagmiConfig>
+  </React.StrictMode>,
+);
